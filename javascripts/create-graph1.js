50e8d89c2e6532de16c08f2a42021c1090366a2c
Udhay <udhay1415@gmail.com>
2017-08-12 09:47:43 -0700

    web-app-v1.0
    version1.0

---
+++ javascripts/create-graph1.js
@@ -0,0 +1,106 @@
+/*
+ * Parse the data and create a graph with the data.
+ */
+function parseData(createGraph) {
+	Papa.parse("../data/sample_data2.csv", {
+		download: true,
+		complete: function(results) {
+			console.log(results.data);
+			createGraph(results.data);
+		}
+	});
+}
+
+function createGraph(data) {
+	var RetentionTime = [];
+	var Intensity = [Intensity];
+
+	for (var i = 0; i < data.length-1; i++) {
+		RetentionTime.push(data[i][0]);
+		Intensity.push(data[i ][1]);
+	}
+
+	console.log(RetentionTime);
+	console.log(Intensity);
+
+	var chart = c3.generate({
+		bindto: '#chart',
+	    data: {
+	        columns: [
+	        	Intensity
+	        ]
+	    },
+	    axis: {
+	        x: {
+	            type: 'category',
+	            categories: RetentionTime,
+	            tick: {
+	            	multiline: false,
+                	culling: {
+                    	max: 15
+                	}
+            	}
+	        }
+	    },
+	    zoom: {
+        	enabled: true
+    	},
+	    legend: {
+	        position: 'right'
+	    }
+	});
+}
+
+parseData(createGraph);
+
+function parseData(createGraph) {
+	Papa.parse("../data/sample_data2.csv", {
+		download: true,
+		complete: function(results) {
+			console.log(results.data);
+			createGraph(results.data);
+		}
+	});
+}
+
+function createGraph(data) {
+	var RetentionTime = [];
+	var Intensity = ["Intensity"];
+
+	for (var i = 1; i < data.length-1; i++) {
+		RetentionTime.push(data[i][0]);
+		Intensity.push(data[i][1]);
+	}
+
+	console.log(RetentionTime);
+	console.log(Intensity);
+
+	var chart = c3.generate({
+		bindto: '#chart1',
+	    data: {
+	        columns: [
+	        	Intensity
+	        ]
+	    },
+	    axis: {
+	        x: {
+	            type: 'category',
+	            categories: RetentionTime,
+	            tick: {
+	            	multiline: false,
+                	culling: {
+                    	max: 15
+                	}
+            	}
+	        }
+	    },
+	    zoom: {
+        	enabled: true
+    	},
+	    legend: {
+	        position: 'right'
+	    }
+	});
+}
+
+parseData(createGraph);
